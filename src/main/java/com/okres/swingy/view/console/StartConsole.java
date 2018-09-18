45cab15c020a8737caf7b5f01c15514cd660dca1
SifuUA <alekseykres@gmail.com>
2018-09-18 23:23:32 +0300

    begin work on console game mode

--- src/main/java/com/okres/swingy/view/console/StartConsole.java
+++ src/main/java/com/okres/swingy/view/console/StartConsole.java
@@ -1,5 +1,8 @@
 package com.okres.swingy.view.console;
 
+import com.okres.swingy.controller.GameController;
+import com.okres.swingy.util.DbUtils;
+
 import java.util.Scanner;
 
 public class StartConsole {
@@ -9,5 +12,17 @@ public class StartConsole {
                 "If you want:", "Play game - input 1", "See score - input 2", "Exit - input 3");
         System.out.println(a);
         int i = scanner.nextInt();
+        switch (i) {
+            case 1:
+                GameController gameController = new GameController();
+                gameController.initializeMap();
+                break;
+            case 2:
+                DbUtils dbUtils = new DbUtils();
+                System.out.println(dbUtils.orderByScore());
+                break;
+            case 3:
+                System.exit(1);
+        }
     }
 }
